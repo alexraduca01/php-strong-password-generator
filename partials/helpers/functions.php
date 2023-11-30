99c853fc1da8d89333d1ac9016cfca8354bf63f3
alexraduca01 <alex.raduca01@icloud.com>
2023-11-30 14:33:26 +0100

    completed Milestone 2

--- partials/helpers/functions.php
+++ partials/helpers/functions.php
@@ -1,5 +1,23 @@
 <?php 
 
-
+function passGen(){
+    $symbols = '!?&%$<>^+-*/()[]{}@#_=';
+    $downLetters = 'qwertyuiopasdfghjklzxcvbnm';
+    $upLetters = strtoupper($downLetters);
+    $numbers = '0123456789';
+    if(isset($_GET['passLength'])){
+        $passwordLength = $_GET['passLength'];
+        $newPass = '';
+        while(strlen($newPass) < $passwordLength){
+            $availableValues = $symbols . $downLetters . $upLetters . $numbers;
+            $newChar = $availableValues[rand(0, strlen($availableValues) - 1)];
+            if(!strpos($newChar, $newChar)){
+                $newPass .= $newChar;
+            }
+        }
+        return $error = $newPass;
+    }
+    return false;
+}
 
 ?>
